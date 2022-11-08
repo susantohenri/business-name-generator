0670f3a94182b82743296fbe81b751f5ca3c06d4
rikzan fernanda <fernandarikzan@gmail.com>
2022-11-08 14:20:48 +0700

    update ui

--- business-name-generator.js
+++ business-name-generator.js
@@ -5,4 +5,15 @@ jQuery(document).ready(function() {
     filterBtn.on('click', function() {
         filterModal.toggleClass('d-none')
     })
+
+    // handle toggle like button
+    jQuery('#business_name_generator .btn-like-outline').on('click', function() {
+        jQuery(this).addClass('d-none')
+        jQuery(this).parent().find('.btn-like-fill').removeClass('d-none')
+    })
+
+    jQuery('#business_name_generator .btn-like-fill').on('click', function() {
+        jQuery(this).addClass('d-none')
+        jQuery(this).parent().find('.btn-like-outline').removeClass('d-none')
+    })
 })
